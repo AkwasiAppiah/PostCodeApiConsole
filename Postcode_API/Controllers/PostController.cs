98fdb496e03e9d5cd6a859e9d1dfc65d069be1b5
AkwasiAppiah <akwasi.atta.appiah@gmail.co.uk>
2021-10-17 22:42:35 +0100

    Finished basic

---
+++ Postcode_API/Controllers/PostController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Postcode_API.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Postcode_API.Controllers
+{
+    public class PostController : Controller
+    {
+
+        public PostController(string url)
+        {
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<string>> PostPostCode(string userPostCode)
+        {
+            using(var client = new HttpClient())
+            {
+
+                string endpoint = $"https://api.postcodes.io/postcodes/{userPostCode}/validate";
+
+                string result = await client.GetAsync(endpoint).Result.Content.ReadAsStringAsync();
+
+                return result;
+            }
+
+        }
+    }
+}
---
+++ Postcode_API/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Postcode_API.Controllers;
+
+namespace Postcode_API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string url = Console.ReadLine();
+
+            PostController test = new PostController(url);
+
+            Task<ActionResult<string>> result = test.PostPostCode(url);
+
+            Console.WriteLine(result);
+            //string userPostCode = Console.ReadLine();
+
+            //    using (var client = new HttpClient())
+            //    {
+
+            //        string endpoint = $"https://api.postcodes.io/postcodes/{userPostCode}/validate";
+
+            //        string result = client.GetAsync(endpoint).Result.Content.ReadAsStringAsync().Result;
+
+            //    Console.WriteLine(result);
+
+            //    Console.Read();
+
+
+            // 1. Create an enpoint
+
+            // 2. Allow it to recieve a any body of json -> dynamic type? 
+
+            // 3. On recieving json parse out postcode from body
+
+            // 4. Make a get request to external api
+
+            // 5. If result is true then return an okay status code else create a status code and send it back.
+
+            
+        }
+    }
+}
