7ba51daa7b6137ab4c3f4331c2b63ec98606b4b5
AkwasiAppiah <akwasi.atta.appiah@gmail.co.uk>
2021-10-17 22:45:27 +0100

    update

---
+++ Postcode_API/Models/ValidPostCode.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Postcode_API.Models
+{
+    public class ValidPostCode
+    {
+        public string success { get; set; }
+        public bool result { get; set; }
+    }
+}
